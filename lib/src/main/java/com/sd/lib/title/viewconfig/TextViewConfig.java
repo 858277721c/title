5d29e527daad4327ce3daddcef48e73dd00b9cac
zjun <565061763@qq.com>
2019-08-30 22:09:46 +0800

    新增setTypeface

--- lib/src/main/java/com/sd/lib/title/viewconfig/TextViewConfig.java
+++ lib/src/main/java/com/sd/lib/title/viewconfig/TextViewConfig.java
@@ -1,5 +1,6 @@
 package com.sd.lib.title.viewconfig;
 
+import android.graphics.Typeface;
 import android.widget.TextView;
 
 public class TextViewConfig<R extends TextViewConfig> extends ViewConfig<TextView, R>
@@ -36,6 +37,21 @@ public class TextViewConfig<R extends TextViewConfig> extends ViewConfig<TextVie
     public R setMaxHeight(int maxPixels)
     {
         getView().setMaxHeight(maxPixels);
+        return (R) this;
+    }
+
+    /**
+     * 设置字体<br>
+     * {@link Typeface#defaultFromStyle(int)}<br>
+     * {@link Typeface#BOLD}<br>
+     * {@link Typeface#NORMAL}<br>
+     *
+     * @param value
+     * @return
+     */
+    public R setTypeface(Typeface value)
+    {
+        getView().setTypeface(value);
         return (R) this;
     }
 }
